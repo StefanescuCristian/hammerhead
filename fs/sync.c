dbb5b78c0cdc854941df3f299afcb224f6c55ff7
Wanpeng Li <liwp@linux.vnet.ibm.com>
2012-06-09 11:10:55 +0800

    writeback: Fix some comment errors
    Signed-off-by: Wanpeng Li <liwp@linux.vnet.ibm.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -115,7 +115,7 @@ EXPORT_SYMBOL_GPL(sync_filesystems);
 #endif
 
 /*
- * sync everything.  Start out by waking pdflush, because that writes back
+ * sync everything.  Start out by waking flusher, because that writes back
  * all queues in parallel.
  */
 static void do_sync(void)
