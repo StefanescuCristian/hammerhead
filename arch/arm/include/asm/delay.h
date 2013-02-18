3f5b2dd89f96e97801757b2831daa2b7e5929250
Nicolas Pitre <nicolas.pitre@linaro.org>
2013-02-18 16:36:13 +0100

    ARM: 7653/2: do not scale loops_per_jiffy when using a constant delay clock
    When udelay() is implemented using an architected timer, it is wrong
    to scale loops_per_jiffy when changing the CPU clock frequency since
    the timer clock remains constant.
    
    The lpj should probably become an implementation detail relevant to
    the CPU loop based delay routine only and more confined to it. In the
    mean time this is the minimal fix needed to have expected delays with
    the timer based implementation when cpufreq is also in use.
    
    Change-Id: I75f66afe0cd4851031c71c1ebd522b402bb0a788
    Reported-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Nicolas Pitre <nico@linaro.org>
    Tested-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Liviu Dudau <Liviu.Dudau@arm.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	arch/arm/include/asm/delay.h

--- arch/arm/include/asm/delay.h
+++ arch/arm/include/asm/delay.h
@@ -24,7 +24,7 @@ extern struct arm_delay_ops {
 	void (*delay)(unsigned long);
 	void (*const_udelay)(unsigned long);
 	void (*udelay)(unsigned long);
-	unsigned long ticks_per_jiffy;
+	bool const_clock;
 } arm_delay_ops;
 
 #define __delay(n)		arm_delay_ops.delay(n)
