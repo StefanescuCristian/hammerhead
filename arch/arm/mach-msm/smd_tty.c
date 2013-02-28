e07f46d5c8f6f28164dd9298db58f98f6e9c2546
Arun Kumar Neelakantam <aneela@codeaurora.org>
2013-02-28 14:13:21 +0530

    msm: smd_tty: add device tree support to smd_tty drivers
    SMD TTY is configured using local tables that do not
    allow customization on a per-target basis.
    
    Add device tree support to configure SMD TTY devices.
    
    Change-Id: Ie02dd63a0bec8edff77c8cbeeb72ae7331a0c67a
    Signed-off-by: Arun Kumar Neelakantam <aneela@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- arch/arm/mach-msm/smd_tty.c
+++ arch/arm/mach-msm/smd_tty.c
@@ -67,6 +67,8 @@ static uint smd_tty_ds_modem_wait = 20;
 module_param_named(ds_modem_wait, smd_tty_ds_modem_wait,
 			uint, S_IRUGO | S_IWUSR | S_IWGRP);
 #endif
+static struct delayed_work smd_tty_probe_work;
+static int smd_tty_probe_done;
 
 struct smd_tty_info {
 	smd_channel_t *ch;
@@ -843,6 +845,7 @@ static int __init smd_tty_init(void)
 				"%s: Unable to create device attributes for %s",
 				__func__, smd_configs[n].port_name);
 
+<<<<<<< HEAD
 		init_completion(&smd_tty[idx].ch_allocated);
 
 		/* register platform device */
@@ -864,6 +867,8 @@ static int __init smd_tty_init(void)
 		init_waitqueue_head(&smd_tty[idx].ch_opened_wait_queue);
 		ret = platform_driver_register(&smd_tty[idx].driver);
 
+=======
+>>>>>>> 99e8872... msm: smd_tty: add device tree support to smd_tty drivers
 		if (ret) {
 			SMD_TTY_ERR(
 				"%s: init failed %d (%d)", __func__, idx, ret);
