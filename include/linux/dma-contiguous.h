bbc27de036ec01877436c17de6d89ca13cc0b6bd
Laura Abbott <lauraa@codeaurora.org>
2013-11-13 11:21:24 -0800

    cma: Add support for removed regions
    In addition to reserving memory from the system, there may
    be uses where memory should be completely removed from control
    of the linux page allocator. Add the appropriate information to
    be able to remove the memory.
    
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	drivers/base/dma-contiguous.c
    	include/linux/dma-contiguous.h

--- include/linux/dma-contiguous.h
+++ include/linux/dma-contiguous.h
@@ -70,7 +70,9 @@ extern struct cma *dma_contiguous_def_area;
 void dma_contiguous_reserve(phys_addr_t addr_limit);
 
 int dma_contiguous_reserve_area(phys_addr_t size, phys_addr_t *res_base,
-				  phys_addr_t limit, const char *name);
+				  phys_addr_t limit, const char *name,
+				  bool in_system,
+				  bool remove);
 
 int dma_contiguous_add_device(struct device *dev, phys_addr_t base);
 
@@ -91,13 +93,27 @@ static inline int dma_declare_contiguous(struct device *dev, phys_addr_t size,
 					 phys_addr_t base, phys_addr_t limit)
 {
 	int ret;
-	ret = dma_contiguous_reserve_area(size, &base, limit, NULL);
+	ret = dma_contiguous_reserve_area(size, &base, limit, NULL, true,
+						false);
 	if (ret == 0)
 		ret = dma_contiguous_add_device(dev, base);
 	return ret;
 }
 
-struct page *dma_alloc_from_contiguous(struct device *dev, int count,
+static inline int dma_declare_contiguous_reserved(struct device *dev,
+					 phys_addr_t size,
+					 phys_addr_t base,
+					 phys_addr_t limit)
+{
+	int ret;
+	ret = dma_contiguous_reserve_area(size, &base, limit, NULL, false,
+						false);
+	if (ret == 0)
+		ret = dma_contiguous_add_device(dev, base);
+	return ret;
+}
+
+unsigned long dma_alloc_from_contiguous(struct device *dev, int count,
 				       unsigned int order);
 bool dma_release_from_contiguous(struct device *dev, struct page *pages,
 				 int count);
