dfdae3efc84bada1f7702e3d40a315630645e3ae
Mekala Natarajan <mekalan@codeaurora.org>
2014-01-28 15:17:41 -0800

    Perf: Support sw events across hotplug
    When CPUs are hotplugged off and on, let the various
    sw events continue after the hotplug.
    
    Change-Id: Id1aaf30c459c9cf7c9c38967f9ccad56d4062fd3
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>
    Signed-off-by: Mekala Natarajan <mekalan@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5330,6 +5330,7 @@ static struct pmu perf_swevent = {
 	.read		= perf_swevent_read,
 
 	.event_idx	= perf_swevent_event_idx,
+	.events_across_hotplug = 1,
 };
 
 #ifdef CONFIG_EVENT_TRACING
@@ -5424,6 +5425,7 @@ static struct pmu perf_tracepoint = {
 	.read		= perf_swevent_read,
 
 	.event_idx	= perf_swevent_event_idx,
+	.events_across_hotplug = 1,
 };
 
 static inline void perf_tp_register(void)
@@ -5651,6 +5653,7 @@ static struct pmu perf_cpu_clock = {
 	.read		= cpu_clock_event_read,
 
 	.event_idx	= perf_swevent_event_idx,
+	.events_across_hotplug = 1,
 };
 
 /*
@@ -5731,6 +5734,7 @@ static struct pmu perf_task_clock = {
 	.read		= task_clock_event_read,
 
 	.event_idx	= perf_swevent_event_idx,
+	.events_across_hotplug = 1,
 };
 
 static void perf_pmu_nop_void(struct pmu *pmu)
