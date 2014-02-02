af047fcc38f4410dbd656d0598c2720761a8ead5
Konstantin Dorfman <kdorfman@codeaurora.org>
2014-02-02 12:49:54 +0200

    mmc: queue: do not clean current request when urgent in progress
    As a result of following fix: "mmc: core: do not reinsert prepeared
    FUA and FLUSH requests in stop flow" REQ_FUA and REQ_FLUSH requests
    are not reinserted back into i/o scheduler, but instead started request
    execution.
    
    This change will prevent cleaning current request pointer for such
    requests.
    
    Change-Id: I25f8706954fb538be62182c87d5fb20354696b7a
    CRs-fixed: 600127
    Signed-off-by: Konstantin Dorfman <kdorfman@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	drivers/mmc/card/block.c

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2708,7 +2708,8 @@ out:
 	 */
 	if ((!req && !(mq->flags & MMC_QUEUE_NEW_REQUEST)) ||
 			((mq->flags & MMC_QUEUE_URGENT_REQUEST) &&
-			 !(cmd_flags & MMC_REQ_NOREINSERT_MASK))) {
+			 !(mq->mqrq_cur->req->cmd_flags &
+				MMC_REQ_NOREINSERT_MASK))) {
 		if (mmc_card_need_bkops(card))
 			mmc_start_bkops(card, false);
 		/* release host only when there are no more requests */
