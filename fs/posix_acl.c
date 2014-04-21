1afaf891f6a1c8b64a6136155f12714dc78f68c6
dennes544 <dennes5443@gmail.com>
2014-04-21 21:24:41 +0400

    F2FS: posix_acl fix

--- fs/posix_acl.c
+++ fs/posix_acl.c
@@ -447,7 +447,7 @@ __posix_acl_chmod(struct posix_acl **acl, gfp_t gfp, umode_t mode)
 	*acl = clone;
 	return err;
 }
-EXPORT_SYMBOL(posix_acl_chmod);
+EXPORT_SYMBOL(__posix_acl_chmod);
 
 int
 posix_acl_chmod(struct inode *inode, umode_t mode)
