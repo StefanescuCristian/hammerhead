2aef78a220bfd4f9a59ead323e5112fe51e662d6
dennes544 <dennes5443@gmail.com>
2014-04-24 19:53:24 +0400

    F2FS: upstream updates

--- fs/f2fs/acl.c
+++ fs/f2fs/acl.c
@@ -224,7 +224,7 @@ static int __f2fs_set_acl(struct inode *inode, int type,
 		}
 	}
 
-	error = f2fs_setxattr(inode, name_index, "", value, size, ipage);
+	error = f2fs_setxattr(inode, name_index, "", value, size, ipage, 0);
 
 	kfree(value);
 	if (!error)
