77d1598aaf475c0d0a40f5b091ef22172a49aa24
Sudheer Papothi <spapothi@codeaurora.org>
2014-05-21 07:13:37 -0700

    ASoC: wcd9xxx: [HACK] Prevent headphone&speaker from clipping
    * Buck mode is set to 2V when HPH+Spk combo device has been selected.
    
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	sound/soc/codecs/wcd9xxx-common.c

--- sound/soc/codecs/wcd9xxx-common.c
+++ sound/soc/codecs/wcd9xxx-common.c
@@ -727,7 +727,7 @@ static void wcd9xxx_set_fclk_get_ncp(struct snd_soc_codec *codec,
 
 	/* enable NCP and wait until settles down */
 	if (snd_soc_update_bits(codec, WCD9XXX_A_NCP_EN, 0x01, 0x01))
-		usleep_range(NCP_SETTLE_TIME_US, NCP_SETTLE_TIME_US);
+		usleep_range(NCP_SETTLE_TIME_US, NCP_SETTLE_TIME_US + 50);
 	pr_debug("%s: leave\n", __func__);
 }
 
@@ -840,6 +840,7 @@ static void wcd9xxx_clsh_state_ear(struct snd_soc_codec *codec,
 		wcd9xxx_clsh_comp_req(codec, clsh_d, CLSH_COMPUTE_EAR, false);
 		wcd9xxx_chargepump_request(codec, false);
 		wcd9xxx_enable_clsh_block(codec, clsh_d, false);
+		clsh_d->state &= ~WCD9XXX_CLSH_STATE_EAR;
 	}
 }
 
@@ -857,7 +858,11 @@ static void wcd9xxx_clsh_state_hph_l(struct snd_soc_codec *codec,
 		wcd9xxx_chargepump_request(codec, true);
 		wcd9xxx_enable_anc_delay(codec, true);
 		wcd9xxx_clsh_comp_req(codec, clsh_d, CLSH_COMPUTE_HPH_L, true);
-		wcd9xxx_set_buck_mode(codec, BUCK_VREF_0P494V);
+		pr_debug("%s class state: %d\n", __func__, clsh_d->state);
+		if (clsh_d->state & WCD9XXX_CLSH_STATE_LO)
+			wcd9xxx_set_buck_mode(codec, BUCK_VREF_2V);
+		else
+			wcd9xxx_set_buck_mode(codec, BUCK_VREF_0P494V);
 		wcd9xxx_enable_buck(codec, clsh_d, true);
 		wcd9xxx_set_fclk_get_ncp(codec, clsh_d, NCP_FCLK_LEVEL_8);
 
@@ -868,6 +873,7 @@ static void wcd9xxx_clsh_state_hph_l(struct snd_soc_codec *codec,
 		wcd9xxx_clsh_comp_req(codec, clsh_d, CLSH_COMPUTE_HPH_L, false);
 		wcd9xxx_chargepump_request(codec, false);
 		wcd9xxx_enable_clsh_block(codec, clsh_d, false);
+		clsh_d->state &= ~WCD9XXX_CLSH_STATE_HPHL;
 	}
 }
 
@@ -885,7 +891,11 @@ static void wcd9xxx_clsh_state_hph_r(struct snd_soc_codec *codec,
 		wcd9xxx_chargepump_request(codec, true);
 		wcd9xxx_enable_anc_delay(codec, true);
 		wcd9xxx_clsh_comp_req(codec, clsh_d, CLSH_COMPUTE_HPH_R, true);
-		wcd9xxx_set_buck_mode(codec, BUCK_VREF_0P494V);
+		pr_debug("%s class state: %d\n", __func__, clsh_d->state);
+		if (clsh_d->state & WCD9XXX_CLSH_STATE_LO)
+			wcd9xxx_set_buck_mode(codec, BUCK_VREF_2V);
+		else
+			wcd9xxx_set_buck_mode(codec, BUCK_VREF_0P494V);
 		wcd9xxx_enable_buck(codec, clsh_d, true);
 		wcd9xxx_set_fclk_get_ncp(codec, clsh_d, NCP_FCLK_LEVEL_8);
 
@@ -896,6 +906,7 @@ static void wcd9xxx_clsh_state_hph_r(struct snd_soc_codec *codec,
 		wcd9xxx_clsh_comp_req(codec, clsh_d, CLSH_COMPUTE_HPH_R, false);
 		wcd9xxx_chargepump_request(codec, false);
 		wcd9xxx_enable_clsh_block(codec, clsh_d, false);
+		clsh_d->state &= ~WCD9XXX_CLSH_STATE_HPHR;
 	}
 }
 
@@ -906,10 +917,24 @@ static void wcd9xxx_clsh_state_hph_st(struct snd_soc_codec *codec,
 	pr_debug("%s: enter %s\n", __func__, is_enable ? "enable" : "disable");
 
 	if (is_enable) {
-		wcd9xxx_clsh_comp_req(codec, clsh_d, CLSH_COMPUTE_HPH_L, true);
-		wcd9xxx_clsh_comp_req(codec, clsh_d,  CLSH_COMPUTE_HPH_R, true);
+		if (req_state == WCD9XXX_CLSH_STATE_HPHL)
+			wcd9xxx_clsh_comp_req(codec, clsh_d,
+						CLSH_COMPUTE_HPH_L, true);
+		if (req_state == WCD9XXX_CLSH_STATE_HPHR)
+			wcd9xxx_clsh_comp_req(codec, clsh_d,
+						CLSH_COMPUTE_HPH_R, true);
+		pr_debug("%s class state: %d\n", __func__, clsh_d->state);
+		if (clsh_d->state & WCD9XXX_CLSH_STATE_LO)
+			wcd9xxx_set_buck_mode(codec, BUCK_VREF_2V);
 	} else {
 		dev_dbg(codec->dev, "%s: stub fallback to hph_st\n", __func__);
+		if (req_state == WCD9XXX_CLSH_STATE_HPHL)
+			wcd9xxx_clsh_comp_req(codec, clsh_d,
+						CLSH_COMPUTE_HPH_L, false);
+		if (req_state == WCD9XXX_CLSH_STATE_HPHR)
+			wcd9xxx_clsh_comp_req(codec, clsh_d,
+						CLSH_COMPUTE_HPH_R, false);
+		clsh_d->state &= ~WCD9XXX_CLSH_STATE_HPH_ST;
 	}
 }
 
@@ -919,8 +944,12 @@ static void wcd9xxx_clsh_state_lo(struct snd_soc_codec *codec,
 {
 	pr_debug("%s: enter %s\n", __func__, is_enable ? "enable" : "disable");
 
+	pr_debug("class state: %d\n", clsh_d->state);
 	if (is_enable) {
-		wcd9xxx_set_buck_mode(codec, BUCK_VREF_1P8V);
+		if (clsh_d->state & WCD9XXX_CLSH_STATE_HPH_ST)
+			wcd9xxx_set_buck_mode(codec, BUCK_VREF_2V);
+		else
+			wcd9xxx_set_buck_mode(codec, BUCK_VREF_1P8V);
 		wcd9xxx_enable_buck(codec, clsh_d, true);
 		wcd9xxx_set_fclk_get_ncp(codec, clsh_d, NCP_FCLK_LEVEL_5);
 
@@ -946,6 +975,7 @@ static void wcd9xxx_clsh_state_lo(struct snd_soc_codec *codec,
 		wcd9xxx_set_fclk_put_ncp(codec, clsh_d, NCP_FCLK_LEVEL_5);
 		if (clsh_d->buck_mv != WCD9XXX_CDC_BUCK_MV_1P8)
 			wcd9xxx_enable_buck(codec, clsh_d, false);
+		clsh_d->state &= ~WCD9XXX_CLSH_STATE_LO;
 	}
 }
 
