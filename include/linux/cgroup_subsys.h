e8268891721f0aff8be2718a28a361b5ec5d9a43
Stefanescu Cristian <howerkraft@gmail.com>
2014-06-02 10:47:15 +0300

    fixing merge derp

--- include/linux/cgroup_subsys.h
+++ include/linux/cgroup_subsys.h
@@ -78,27 +78,3 @@ SUBSYS(bfqio)
 #endif
 
 /* */
-
-#ifdef CONFIG_CGROUP_BFQIO
-SUBSYS(bfqio)
-#endif
-
-/* */
-
-#ifdef CONFIG_CGROUP_BFQIO
-SUBSYS(bfqio)
-#endif
-
-/* */
-
-#ifdef CONFIG_CGROUP_BFQIO
-SUBSYS(bfqio)
-#endif
-
-/* */
-
-#ifdef CONFIG_CGROUP_BFQIO
-SUBSYS(bfqio)
-#endif
-
-/* */
