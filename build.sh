bb58e787d32e3679b35cf6bb80a7f172b1cdd987
Stefanescu Cristian <howerkraft@gmail.com>
2014-06-09 14:20:29 +0300

    Adding support for building f2fs kernel ramdisks. Thanks _MirageX_ from XDA for testing

--- build.sh
+++ build.sh
@@ -11,10 +11,26 @@ cp arch/arm/boot/zImage-dtb ../ramdisk_hammerhead/
 
 cd ../ramdisk_hammerhead/
 
+
+git checkout master
 echo "making ramdisk"
 ./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
-echo "making boot image"
+echo "making normal boot image"
 ./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot.img
-
 rm -rf ramdisk.gz
-rm -rf zImage
+
+git checkout f2fs
+echo "making f2fs ramdisk"
+./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
+echo "making normal boot image"
+./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-f2fs.img
+rm -rf ramdisk.gz
+
+git checkout f2fs-all
+echo "making f2fs-all ramdisk"
+./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
+echo "making normal boot image"
+./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-f2fs-all.img
+rm -rf ramdisk.gz
+
+rm -rf zImage*
