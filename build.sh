2f1025b162f96c2d92f08ce55f03120667da6b8e
Stefanescu Cristian <howerkraft@gmail.com>
2014-06-15 15:08:23 +0300

    zipfiles for each version

--- build.sh
+++ build.sh
@@ -13,26 +13,28 @@ cp arch/arm/boot/zImage-dtb ../ramdisk_hammerhead/
 
 cd ../ramdisk_hammerhead/
 
-
+version=$(cat ../hammerhead/.version)
 git checkout master
 echo "making ramdisk"
 ./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
 echo "making normal boot image"
-./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot.img
+./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-v"$version".img
 rm -rf ramdisk.gz
 
 git checkout f2fs
 echo "making f2fs ramdisk"
 ./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
 echo "making f2fs boot image"
-./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-f2fs.img
+./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-f2fs-v"$version".img
 rm -rf ramdisk.gz
 
 git checkout f2fs-all
 echo "making f2fs-all ramdisk"
 ./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
 echo "making f2fs-all boot image"
-./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-f2fs-all.img
+./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-f2fs-all-v"$version".img
 rm -rf ramdisk.gz
 
 rm -rf zImage*
+
+./zip.sh
---
+++ zip.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+zipname="big-bum"
+version=$(cat .version)
+cd zip/
+rm -f ../*.zip
+
+#normal version
+cp ../boot-v"$version".img boot.img
+zip -r -9 "$zipname"-v"$version".zip *
+mv *.zip ../
+rm *.img
+
+#f2fs
+cp ../boot-f2fs-v"$version".img boot.img
+zip -r -9 "$zipname"-f2fs-v"$version".zip *
+mv *.zip ../
+rm *.img
+
+#f2fs-all
+cp ../boot-f2fs-all-v"$version".img boot.img
+zip -r -9 "$zipname"-f2fs-all-v"$version".zip *
+mv *.zip ../
+rm *.img
