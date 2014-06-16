3ae564aaced71443b8f32119cae404732632f72a
Stefanescu Cristian <howerkraft@gmail.com>
2014-06-16 11:19:49 +0300

    Revert "msm: mdss: control backlight level during blank and unblank events"
    This reverts commit 20e13edec2ebd9af85ccb3a28102ec89ceae25c8.

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -153,8 +153,7 @@ struct msm_fb_data_type {
 	u32 bl_min_lvl;
 	u32 unset_bl_level;
 	u32 bl_updated;
-	u32 bl_level_scaled;
-	u32 bl_level_prev_scaled;
+	u32 bl_level_old;
 	struct mutex bl_lock;
 	struct mutex lock;
 
