562420b9fd12ac6b7be448f50c7777aeb8773ba1
Stefanescu Cristian <howerkraft@gmail.com>
2014-06-19 10:58:36 +0300

    Make use of glibc versions of memcopy,memmove. Taken from here https://github.com/faux123/Nexus_5/issues/6

--- arch/arm/include/asm/string.h
+++ arch/arm/include/asm/string.h
@@ -12,11 +12,11 @@ extern char * strrchr(const char * s, int c);
 #define __HAVE_ARCH_STRCHR
 extern char * strchr(const char * s, int c);
 
-#define __HAVE_ARCH_MEMCPY
+/*#define __HAVE_ARCH_MEMCPY
 extern void * memcpy(void *, const void *, __kernel_size_t);
 
 #define __HAVE_ARCH_MEMMOVE
-extern void * memmove(void *, const void *, __kernel_size_t);
+extern void * memmove(void *, const void *, __kernel_size_t);*/
 
 #define __HAVE_ARCH_MEMCHR
 extern void * memchr(const void *, int, __kernel_size_t);
