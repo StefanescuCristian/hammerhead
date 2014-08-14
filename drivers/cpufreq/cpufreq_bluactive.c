d6c97eed747d7fad455e1a85c6f89cea01b38ab9
engstk <eng.stk@sapo.pt>
2014-08-14 23:12:26 +0100

    bluactive: don't scale beyond min if the load is low
    by franciscofranco
    
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- drivers/cpufreq/cpufreq_bluactive.c
+++ drivers/cpufreq/cpufreq_bluactive.c
@@ -139,6 +139,8 @@ static unsigned int up_threshold_any_cpu_load;
 static unsigned int sync_freq;
 static unsigned int up_threshold_any_cpu_freq;
 
+#define DOWN_LOW_LOAD_THRESHOLD 5
+
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
 						  cputime64_t *wall)
 {
@@ -480,7 +482,11 @@ static void cpufreq_interactive_timer(unsigned long data)
 			if (new_freq < boosted_freq)
 				new_freq = boosted_freq;
 		}
-	} else {
+	}
+	else if (cpu_load <= DOWN_LOW_LOAD_THRESHOLD) {
+				new_freq = pcpu->policy->cpuinfo.min_freq;
+	}
+	else {
 		new_freq = calc_freq(pcpu, cpu_load);
 		if (new_freq > boosted_freq &&
 				pcpu->target_freq < boosted_freq)
