8f61b577df6864674d5b5fdb27b5f072b266f4d5
alucard24 <dmbaoh2@gmail.com>
2014-08-30 02:25:49 +0200

    System adapted to new workqueue 3.7
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>
    
    Conflicts:
    	drivers/md/dm-mpath.c
    	drivers/usb/otg/msm_otg.c
    	drivers/video/msm/mdss/mdss_hdmi_tx.c

--- drivers/block/floppy.c
+++ drivers/block/floppy.c
@@ -4321,7 +4321,7 @@ static int __init floppy_init(void)
 out_unreg_platform_dev:
 	platform_device_unregister(&floppy_device[drive]);
 out_flush_work:
-	flush_work_sync(&floppy_work);
+	flush_work(&floppy_work);
 	if (atomic_read(&usage_count))
 		floppy_release_irq_and_dma();
 out_unreg_region:
@@ -4397,7 +4397,7 @@ static int floppy_grab_irq_and_dma(void)
 	 * We might have scheduled a free_irq(), wait it to
 	 * drain first:
 	 */
-	flush_work_sync(&floppy_work);
+	flush_work(&floppy_work);
 
 	if (fd_request_irq()) {
 		DPRINT("Unable to grab IRQ%d for the floppy driver\n",
