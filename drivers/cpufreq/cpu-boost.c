dorimanx/Dorimanx-LG-G2-D802-Kernel

cpufreq: cpuboost: Sanity optimization for hotplug boost
* Don't check if hotplug_boost is enabled for every event.
  We need to check same only when CPU_ONLINE event occurs.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

drivers/cpufreq/cpu-boost.c
@@ -388,23 +388,20 @@ static int cpuboost_cpu_callback(struct notifier_block *cpu_nb,
 	int ret;
 	struct cpufreq_policy policy;
 
-	if (!cpu_boost)
-		return NOTIFY_OK;
-
-	ret = cpufreq_get_policy(&policy, 0);
-	if (!ret && input_boost_freq <= policy.cpuinfo.min_freq)
-		return NOTIFY_OK;
-
 	switch (action & ~CPU_TASKS_FROZEN) {
 	case CPU_UP_PREPARE:
 	case CPU_DEAD:
 	case CPU_UP_CANCELED:
 		break;
 	case CPU_ONLINE:
-		if (work_pending(&input_boost_work))
+		if (!cpu_boost || !input_boost_freq ||
+		     work_pending(&input_boost_work))
 			break;
-		pr_debug("Input boost for CPU%d\n", (int)hcpu);
+		ret = cpufreq_get_policy(&policy, 0);
+		if (!ret && input_boost_freq <= policy.cpuinfo.min_freq)
+			break;
 		queue_work(cpu_boost_wq, &input_boost_work);
+		pr_debug("Input boost for CPU%d\n", (int)hcpu);
 		last_input_time = ktime_to_us(ktime_get());
 		break;
 	default:

StefanescuCristian/hammerhead

Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

drivers/cpufreq/cpu-boost.c
@@ -362,16 +362,14 @@ static struct input_handler cpuboost_input_handler = {
 static int cpuboost_cpu_callback(struct notifier_block *cpu_nb,
 				 unsigned long action, void *hcpu)
 {
-	if (!hotplug_boost || !input_boost_freq)
-		return NOTIFY_OK;
-
 	switch (action & ~CPU_TASKS_FROZEN) {
 	case CPU_UP_PREPARE:
 	case CPU_DEAD:
 	case CPU_UP_CANCELED:
 		break;
 	case CPU_ONLINE:
-		if (work_pending(&input_boost_work))
+		if (!hotplug_boost || !input_boost_freq ||
+		     work_pending(&input_boost_work))
 			break;
 		pr_debug("Hotplug boost for CPU%d\n", (int)hcpu);
 		queue_work(cpu_boost_wq, &input_boost_work);