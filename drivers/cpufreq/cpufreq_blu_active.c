bd0ad1003f781f7e56279f5a8a2a313712912f0e
engstk <eng.stk@sapo.pt>
2014-09-17 02:39:33 +0100

    blu_active: use get_cpu_idle_time_cb
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_blu_active.c

--- drivers/cpufreq/cpufreq_blu_active.c
+++ drivers/cpufreq/cpufreq_blu_active.c
@@ -166,7 +166,7 @@ static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
 	return jiffies_to_usecs(idle_time);
 }
 
-static inline cputime64_t get_cpu_idle_time_blu(unsigned int cpu,
+static inline cputime64_t get_cpu_idle_time_cb(unsigned int cpu,
 					    cputime64_t *wall)
 {
 	u64 idle_time = get_cpu_idle_time_us(cpu, wall);
@@ -188,7 +188,7 @@ static void cpufreq_interactive_timer_resched(unsigned long cpu)
 
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
-		get_cpu_idle_time_blu(smp_processor_id(),
+		get_cpu_idle_time_cb(smp_processor_id(),
 				     &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
@@ -234,7 +234,7 @@ static void cpufreq_interactive_timer_start(int cpu)
 	}
 
 	pcpu->time_in_idle =
-		get_cpu_idle_time_blu(cpu, &pcpu->time_in_idle_timestamp);
+		get_cpu_idle_time_cb(cpu, &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
 	spin_unlock_irqrestore(&pcpu->load_lock, flags);
@@ -276,7 +276,7 @@ static u64 update_load(int cpu)
 	unsigned int delta_time;
 	u64 active_time;
 
-	now_idle = get_cpu_idle_time_blu(cpu, &now);
+	now_idle = get_cpu_idle_time_cb(cpu, &now);
 	delta_idle = (unsigned int)(now_idle - pcpu->time_in_idle);
 	delta_time = (unsigned int)(now - pcpu->time_in_idle_timestamp);
 
