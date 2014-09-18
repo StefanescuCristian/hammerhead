99127a4e98a6c2bbb2e12122e679a0d8ee2e5626
Stefanescu Cristian <howerkraft@gmail.com>
2014-09-18 23:08:14 +0300

    build.sh: changes for mpdecision
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- build.sh
+++ build.sh
@@ -18,14 +18,13 @@ cp arch/arm/boot/zImage-dtb ../ramdisk_hammerhead/
 cd ../ramdisk_hammerhead/
 
 version=$(cat ../hammerhead/.version)
-for branch in ext4 f2fs f2fs-all; do
+branch=mpd
 	git checkout $branch
 	echo "making $branch ramdisk"
 	./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
 	echo "making $branch boot image"
 	./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-"$branch"-v"$version".img
 	rm -rf ramdisk.gz
-done
 
 rm -rf zImage*
 
