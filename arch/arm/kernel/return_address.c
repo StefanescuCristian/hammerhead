46f3951e95a0aafc14062ea046b45ce5e4eaa927
Paul Beeler <pbeeler80@gmail.com>
2014-10-22 00:06:20 -0600

    ARM: return_address:  Use the unwind tables from the compiler
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -59,7 +59,8 @@ void *return_address(unsigned int level)
 #else /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) */
 
 #if defined(CONFIG_ARM_UNWIND)
-#warning "TODO: return_address should use unwind tables"
+/* Include unwind tables from arm-eabi toolchain and kernel source */
+#include <unwind.h>
 #endif
 
 
