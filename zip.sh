ac3b5014022c9f92256152613de91b449a7bd97f
Stefanescu Cristian <howerkraft@gmail.com>
2014-12-23 15:34:31 +0200

    zip.sh: update
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- zip.sh
+++ zip.sh
@@ -2,14 +2,10 @@
 zipname="big-bum"
 version=$(cat .version)
 cd zip/
-rm -f ../*.zip
-
-for branch in L L-f2fs L-f2fs-all; do
-	cp ../boot-"$branch"-v"$version".img boot.img
-	zip -q -r -9 "$zipname"-"$branch"-v"$version".zip *
-	mv *.zip ../
-	rm *.img
-	if [ -e ../"$zipname"-"$branch"-v"$version".zip ]; then
-		echo "$branch zip made"
-	fi
-done
+cp ../boot-L-v"$version".img boot.img
+zip -q -r -9 "$zipname"-L-v"$version".zip *
+mv *.zip ../
+rm *.img
+if [ -e ../"$zipname"-L-v"$version".zip ]; then
+	echo "zip made"
+fi
