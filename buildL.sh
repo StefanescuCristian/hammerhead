1e446498bbdea8a54a0669e5793e03dba3138d7a
Stefanescu Cristian <howerkraft@gmail.com>
2014-12-23 15:28:57 +0200

    build.sh: remove unneeded branches
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- buildL.sh
+++ buildL.sh
@@ -17,12 +17,9 @@ cd ../ramdisk_hammerhead/
 
 version=$(cat ../hammerhead/.version)
 
-for branch in L L-f2fs L-f2fs-all; do
-	git checkout $branch
-	./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
-	./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-"$branch"-v"$version".img
-	rm -rf ramdisk.gz
-done
+./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
+./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-L-v"$version".img
+rm -rf ramdisk.gz
 
 rm -rf zImage*
 
