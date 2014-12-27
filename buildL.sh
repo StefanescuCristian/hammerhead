2ab8268ec605e52f89cc96b336f153a1cb5e5a55
Stefanescu Cristian <howerkraft@gmail.com>
2014-12-27 20:35:56 +0200

    boot with selinux permissive
    FYRITP SELinux
    
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- buildL.sh
+++ buildL.sh
@@ -18,7 +18,7 @@ cd ../ramdisk_hammerhead/
 version=$(cat ../hammerhead/.version)
 
 ./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
-./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-L-v"$version".img
+./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead androidboot.selinux=permissive user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-L-v"$version".img
 rm -rf ramdisk.gz
 
 rm -rf zImage*
