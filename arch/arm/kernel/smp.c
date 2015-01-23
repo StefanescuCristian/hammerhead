50561e7b268fe5cfb9710b07ce76179069bf5d1d
Stefanescu Cristian <howerkraft@gmail.com>
2015-01-23 23:11:37 +0200

    Revert "ARM: 7587/1: implement optimized percpu variable access"
    This reverts commit e7de125ff310a585a8e922ec5fc4a268e6338d41.

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -356,10 +356,9 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	enter_lazy_tlb(mm, current);
 	local_flush_tlb_all();
 
-	cpu_init();
-
 	pr_debug("CPU%u: Booted secondary processor\n", cpu);
 
+	cpu_init();
 	preempt_disable();
 	trace_hardirqs_off();
 
@@ -413,7 +412,9 @@ void __init smp_cpus_done(unsigned int max_cpus)
 
 void __init smp_prepare_boot_cpu(void)
 {
-	set_my_cpu_offset(per_cpu_offset(smp_processor_id()));
+	unsigned int cpu = smp_processor_id();
+
+	per_cpu(cpu_data, cpu).idle = current;
 }
 
 void __init smp_prepare_cpus(unsigned int max_cpus)
