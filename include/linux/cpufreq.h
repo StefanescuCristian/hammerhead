c0feec3ffe2b5a3de5db65a8457ebe62ad9e3fa1
Pranav Vashi <neobuddy89@gmail.com>
2015-02-04 21:42:46 +0530

    cpufreq: Add Impulse governor
    * Experimental.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    cpufreq: impulse: don't skip waking up speedchange_task
     if target_freq > policy->cur
    
    When __cpufreq_driver_target() in speedchange_task failed for some reason, the
    policy->cur could be lower than the target_freq. The governor misses to change
    the target_freq if the target_freq is equal to the next_freq at the next sample
    time.
    
    Added a check to prevent the CPU to stay at the speed that is lower than the
    target_freq for long duration.
    
    Signed-off-by: Minsung Kim <ms925.kim@samsung.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>
    
    cpufreq: impulse: Skip boost if already boosted
    
    * Do not set boostpulse if CPU Boost driver is
      alraedy providing boost to the core.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>
    
    cpufreq: impulse: Clean up freq selection
    
    * Always compute new freq and then check if boost
      is required.
    * While checking for boost, consider CPU Boost as
      one of the case.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>
    
    cpufreq: impulse: Add go_lowspeed_load
    
    * If load is too low, switch to lowest freq.
    * Also, consider CPU load greater than hispeed load
      as boosted case.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>
    
    cpufreq: impulse: Add powersave_bias
    
    * powersave_bias when enabled will make use of
      closest freq selection in speedchange task instead
      of selecting higher freq.
    * Since this needs some testing, we will keep it
      disabled by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>
    
    cpufreq: impulse: always use CPUFREQ_RELATION_C
    
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>
    
    cpufreq: impulse: Clean up
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -378,6 +378,8 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 
