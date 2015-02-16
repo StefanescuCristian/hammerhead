8cc7a5676da51f95a9cc6fe4090ebfee294fdbf9
Stefanescu Cristian <howerkraft@gmail.com>
2015-02-16 19:35:13 +0200

    build: max compress for initram
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- buildSM.sh
+++ buildSM.sh
@@ -19,7 +19,7 @@ cd ../ramdisk_hammerhead/
 
 version=$(cat ../hammerhead/.version)
 
-./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
+./mkbootfs boot.img-ramdisk | gzip -9 > ramdisk.gz
 ./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead androidboot.selinux=permissive user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-L-v"$version"-SM.img
 rm -rf ramdisk.gz
 
