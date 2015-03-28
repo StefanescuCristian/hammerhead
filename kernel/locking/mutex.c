68abe114d51c2a3da16710864ea28aac3f94f376
Stefanescu Cristian <howerkraft@gmail.com>
2015-03-29 00:14:44 +0200

    mutex LTO fixes
    https://github.com/andikleen/linux-misc/commit/e2fe20eadaf0375874c3e656b94571733d0187af
    
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- kernel/locking/mutex.c
+++ kernel/locking/mutex.c
@@ -66,7 +66,7 @@ EXPORT_SYMBOL(__mutex_init);
  * We also put the fastpath first in the kernel image, to make sure the
  * branch is predicted by the CPU as default-untaken.
  */
-__visible void __sched __mutex_lock_slowpath(atomic_t *lock_count);
+asmlinkage void __sched __mutex_lock_slowpath(atomic_t *lock_count);
 
 /**
  * mutex_lock - acquire the mutex
@@ -223,8 +223,7 @@ static inline int mutex_can_spin_on_owner(struct mutex *lock)
 }
 #endif
 
-__visible __used noinline
-void __sched __mutex_unlock_slowpath(atomic_t *lock_count);
+asmlinkage void __sched __mutex_unlock_slowpath(atomic_t *lock_count);
 
 /**
  * mutex_unlock - release the mutex
@@ -745,7 +744,7 @@ __mutex_unlock_common_slowpath(atomic_t *lock_count, int nested)
 /*
  * Release the lock, slowpath:
  */
-__visible void
+asmlinkage void
 __mutex_unlock_slowpath(atomic_t *lock_count)
 {
 	__mutex_unlock_common_slowpath(lock_count, 1);
@@ -802,7 +801,7 @@ int __sched mutex_lock_killable(struct mutex *lock)
 }
 EXPORT_SYMBOL(mutex_lock_killable);
 
-__visible void __sched
+asmlinkage void __sched
 __mutex_lock_slowpath(atomic_t *lock_count)
 {
 	struct mutex *lock = container_of(lock_count, struct mutex, count);
