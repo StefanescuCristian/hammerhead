d78874832df81a64bc8192d18099e6a603b2d17e
Stefanescu Cristian <howerkraft@gmail.com>
2015-04-14 20:30:21 +0300

    change name accordingly
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- buildLinaro.sh
+++ buildLinaro.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Linaro - https://github.com/UBERTC/
 export CROSS_COMPILE="ccache ../arm-eabi-4.9-cortex-a15/bin/arm-eabi-"
-echo "LN" > .tc
-rm *Linaro.img *Linaro.zip
+echo "UTC" > .tc
+rm *.img *.zip
 
 make mrproper
 make big-bum_defconfig
@@ -20,7 +20,7 @@ git checkout L
 version=$(cat ../hammerhead/.version)
 
 ./mkbootfs boot.img-ramdisk | gzip -9 > ramdisk.gz
-./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead androidboot.selinux=permissive user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-L-v"$version"-Linaro.img
+./mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead androidboot.selinux=permissive user_debug=31 msm_watchdog_v2.enable=1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output ../hammerhead/boot-L-v"$version"-UTC.img
 rm -rf ramdisk.gz
 
 rm -rf zImage*
