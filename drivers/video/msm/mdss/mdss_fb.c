d6b17b4a1df24610a12d23b351a9aabd05ad53e4
Stefanescu Cristian <howerkraft@gmail.com>
2015-10-05 20:15:05 +0300

    Revert "msm: mdss: Avoid calling wake_up_all() twice in display thread"
    This reverts commit 49398c747256ddcc4b7b04ce8f6d2f7d212d72a4.

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1628,7 +1628,6 @@ static int __mdss_fb_display_thread(void *data)
 	}
 
 	atomic_set(&mfd->commits_pending, 0);
-	atomic_set(&mfd->kickoff_pending, 0);
 	wake_up_all(&mfd->idle_wait_q);
 
 	return ret;
