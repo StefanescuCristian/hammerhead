3d34f6d00312abe827ad9e0c0e8a1e484954f5dc
Stefanescu Cristian <howerkraft@gmail.com>
2015-10-11 01:10:31 +0300

    fix merge for partial_resume
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -337,8 +337,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 
 	do {
 		error = suspend_enter(state, &wakeup);
-	} while (!error && !wakeup && need_suspend_ops(state)
-		&& suspend_ops->suspend_again && suspend_ops->suspend_again());
+	} while (!error && !wakeup && need_suspend_ops(state) && suspend_again(&resumed));
 
  Resume_devices:
 	suspend_test_start();
